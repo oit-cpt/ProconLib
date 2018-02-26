c755613dffb3d0f553d48c32ab52962a9e083ab1
odan <odan3240@gmail.com>
2018-02-26 19:37:09 +0900

    apply clang-format

--- lib/math/inverse_factorial.cpp
+++ lib/math/inverse_factorial.cpp
@@ -1,11 +1,11 @@
 #include "template.h"
 
 vector<Int> calcInverseFactorial(Int N, Int MOD) {
-    vector<Int> factr(N);
-    vector<Int> inv(N);
-    inv[1] = 1;
-    factr[0] = 1;
-    rep(i, N) if (i > 1) inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD;
-    rep(i, N) if (i) factr[i] = factr[i - 1] * inv[i] % MOD;
-    return factr;
+  vector<Int> factr(N);
+  vector<Int> inv(N);
+  inv[1] = 1;
+  factr[0] = 1;
+  rep(i, N) if (i > 1) inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD;
+  rep(i, N) if (i) factr[i] = factr[i - 1] * inv[i] % MOD;
+  return factr;
 }
