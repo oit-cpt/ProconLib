1549b3f7fc8b03d7167413531feb3c218b47cc1e
wheson <wheson.hxs@gmail.com>
2018-03-31 20:42:42 +0900

    綺麗にした

--- lib/Graph/dijkstra.cpp
+++ lib/Graph/dijkstra.cpp
@@ -1,9 +1,8 @@
 #include "template.h"
 
 struct Edge {
-  Int cost;
-  int to;
-  Edge(int t, Int c) {
+  int cost, to;
+  Edge(int t, int c) {
     cost = c;
     to = t;
   }
@@ -14,30 +13,30 @@ struct Edge {
 class Dijkstra {
  private:
   bool isDir = false;  // 無向グラフ: false, 有向グラフ: true
-  Int INFl = (Int)1e15;
-  int arraySizeOfCost;
+  long long INFl = (long long)1e15;
+  int V;                     // 頂点数
   vector<vector<Edge>> adj;  // adj[始点][動的配列で始点から伸びる枝]
+  vector<int> prever;
 
  public:
   Dijkstra(int n, bool dir);
-  vector<Int> cost;
-  vector<int> prever;
-  void AddEdge(int f, int t, Int c);
-  bool HasPath(int t);  // tに至るパスはあるか
-  vector<int> getShortestPath(int t);
+  vector<long long> cost;
+  void AddEdge(int f, int t, int c);
+  bool HasPath(int t);                 // tに至るパスはあるか
+  vector<int> getShortestPath(int t);  // tへの最短路
   void Run(int f);
 };
 
 Dijkstra::Dijkstra(int n, bool dir)
     : isDir(dir),
-      arraySizeOfCost(n + 1),
-      adj(vector<vector<Edge>>(arraySizeOfCost)),
-      cost(vector<Int>(arraySizeOfCost)),
-      prever(vector<int>(arraySizeOfCost, -1)) {
+      V(n + 1),
+      adj(vector<vector<Edge>>(V)),
+      prever(vector<int>(V, -1)),
+      cost(vector<long long>(V)) {
   fill(cost.begin(), cost.end(), INFl);
 }
 
-void Dijkstra::AddEdge(int f, int t, Int c) {
+void Dijkstra::AddEdge(int f, int t, int c) {
   adj[f].push_back(Edge(t, c));
   if (!isDir) adj[t].push_back(Edge(f, c));
 }
@@ -52,26 +51,23 @@ vector<int> Dijkstra::getShortestPath(int t) {
   return path;
 }
 
-void Dijkstra::Run(int first_state) {
-  using State = Edge;
-  priority_queue<State, vector<State>, greater<State>> pq;
+void Dijkstra::Run(int firstNode) {
+  priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
 
-  cost[first_state] = 0;
-  pq.push(Edge(first_state, 0LL));
+  cost[firstNode] = 0;
+  pq.push(Edge(firstNode, 0LL));
 
   while (!pq.empty()) {
-    State current_state = pq.top();
+    Edge currentEdge = pq.top();
     pq.pop();
 
-    if (cost[current_state.to] < current_state.cost) continue;
+    if (cost[currentEdge.to] < currentEdge.cost) continue;
 
-    for (int i = 0; i < adj[current_state.to].size(); i++) {
-      State tmp = adj[current_state.to][i];
-
-      Int sum_cost = current_state.cost + tmp.cost;
-      if (cost[tmp.to] > sum_cost) {
-        cost[tmp.to] = sum_cost;
-        prever[tmp.to] = current_state.to;
+    for (Edge tmp : adj[currentEdge.to]) {
+      long long sumCost = currentEdge.cost + tmp.cost;
+      if (cost[tmp.to] > sumCost) {
+        cost[tmp.to] = sumCost;
+        prever[tmp.to] = currentEdge.to;
         pq.push(Edge(tmp.to, cost[tmp.to]));
       }
     }
