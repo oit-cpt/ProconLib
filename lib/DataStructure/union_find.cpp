a7bfbcbcd87cfb68d021a3e827e5d50938048ab5
wheson <wheson.hxs@gmail.com>
2018-03-31 20:04:31 +0900

    make formatした

--- lib/DataStructure/union_find.cpp
+++ lib/DataStructure/union_find.cpp
@@ -5,10 +5,10 @@ class UnionFind {
   vector<int> parent;  // 親
   vector<int> rank;    // 木の深さ
  public:
-  explicit UnionFind(int n); // n要素で初期化
-  int Find(int x);           // 木の根を返す
-  void Unite(int x, int y);  // xとyの属する集合を併合
-  bool Same(int x, int y);   // xとyが同じ集合に属するか否か
+  explicit UnionFind(int n);  // n要素で初期化
+  int Find(int x);            // 木の根を返す
+  void Unite(int x, int y);   // xとyの属する集合を併合
+  bool Same(int x, int y);    // xとyが同じ集合に属するか否か
 };
 
 UnionFind::UnionFind(int n) : parent(vector<int>(n)), rank(vector<int>(n)) {
