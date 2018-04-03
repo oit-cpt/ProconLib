3652933cd8e67d56fb61ab4ed5bf517f1f035b87
wheson <wheson.hxs@gmail.com>
2018-04-03 10:19:59 +0900

    graph.hの追加

---
+++ include/graph.h
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+struct Edge {
+  int src, to;
+  T cost;
+  Edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+  Edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+};
+
+template <typename T>
+using Edges = vector<Edge<T>>;
+template <typename T>
+using WeightedGraph = vector<Edges<T>>;
