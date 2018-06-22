6aedbaf13cb6b569db2c0ae5f23bf0a0329d16ba
wheson <wheson.hxs@gmail.com>
2018-06-23 08:01:03 +0900

    make format

--- lib/Math/eratos.h
+++ lib/Math/eratos.h
@@ -2,13 +2,16 @@
 #include "template.h"
 
 template <typename T>
-vector<T> eratos(T n){
-    vector<T> primeVec;
-    vector<bool> isPrime(n+1, true);
-    isPrime[0] = false; isPrime[1] = false;
-    for(int p = 2; p*p <= n; p++){
-        if(isPrime[p]) for(int i = p*2; i <= n; i += p) isPrime[i] = false;
-    }
-    for(int p = 2; p <= n; p++) if(isPrime[p]) primeVec.pb(p);
-    return primeVec;
+vector<T> eratos(T n) {
+  vector<T> primeVec;
+  vector<bool> isPrime(n + 1, true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (int p = 2; p * p <= n; p++) {
+    if (isPrime[p])
+      for (int i = p * 2; i <= n; i += p) isPrime[i] = false;
+  }
+  for (int p = 2; p <= n; p++)
+    if (isPrime[p]) primeVec.pb(p);
+  return primeVec;
 }
