a52759a8ddd2bec4b88af117c36816d178839f6f
wheson <wheson.hxs@gmail.com>
2018-06-24 11:56:21 +0900

    test関数の引数の修正

--- test/Math/test_eratos.cpp
+++ test/Math/test_eratos.cpp
@@ -2,14 +2,14 @@
 #include "template.h"
 vector<int> eratos(int N);
 
-TEST(MathTest, eratos) {
+TEST(MathTest, eratos_100) {
   const int N = 100;
   vector<int> fact = {2,  3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37, 41,
                       43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
   auto sosuuList = eratos(N);
   rep(i, sosuuList.size()) { EXPECT_EQ(fact[i], sosuuList[i]); }
 }
-TEST(MathTest, eratos) {
+TEST(MathTest, eratos_10000) {
   const int N = 10000;
   int fact = 1229;
   auto sosuuList = eratos(N);
