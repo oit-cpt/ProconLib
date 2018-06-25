20c16fdcd96fa8a5758077de1d782d05102bfc47
wheson <wheson.hxs@gmail.com>
2018-06-25 14:33:30 +0900

    sosuuList -> primeVec

--- test/Math/test_eratos.cpp
+++ test/Math/test_eratos.cpp
@@ -5,12 +5,12 @@ TEST(MathTest, eratos_100) {
   const int N = 100;
   vector<int> fact = {2,  3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37, 41,
                       43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-  auto sosuuList = eratos(N);
-  rep(i, sosuuList.size()) { EXPECT_EQ(fact[i], sosuuList[i]); }
+  auto primeVec = eratos(N);
+  rep(i, primeVec.size()) { EXPECT_EQ(fact[i], primeVec[i]); }
 }
 TEST(MathTest, eratos_10000) {
   const int N = 10000;
   int fact = 1229;
-  auto sosuuList = eratos(N);
-  EXPECT_EQ(fact, (int)sosuuList.size());
+  auto primeVec = eratos(N);
+  EXPECT_EQ(fact, (int)primeVec.size());
 }
