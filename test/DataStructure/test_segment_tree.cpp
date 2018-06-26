1195647c778da8b69fae6e5525a9a63bf04920d4
wheson <wheson.hxs@gmail.com>
2018-06-26 20:33:56 +0900

    単一更新区間取得セグメント木のテストを作成(RMQ, RSQ)

---
+++ test/DataStructure/test_segment_tree.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+#include "lib/DataStructure/segment_tree.h"
+
+// Range Minimum Query
+TEST(DataStructureTest, segment_tree_RMQ){
+    vector<int> vec(3, (2 << 31) - 1);
+    SegmentTree<int> segmentTree(vec, [](int a, int b){ return min(a, b); }, (2 << 31) - 1);
+
+    segmentTree.Update(0, 1);
+    segmentTree.Update(1, 2);
+    segmentTree.Update(2, 3);
+
+    EXPECT_EQ(segmentTree.Query(0, 2+1), 1);
+    EXPECT_EQ(segmentTree.Query(1, 2+1), 2);
+}
+
+// Range Sum Query
+TEST(DataStructureTest, segment_tree_RSQ){
+    vector<int> vec(3, 0);
+    SegmentTree<int> segmentTree(vec, [](int a, int b){ return a + b; }, 0);
+
+    segmentTree.Update(0, 1);
+    segmentTree.Update(1, 2);
+    segmentTree.Update(2, 3);
+
+    EXPECT_EQ(segmentTree.Query(0, 1+1), 3);
+    EXPECT_EQ(segmentTree.Query(1, 1+1), 2);
+}
